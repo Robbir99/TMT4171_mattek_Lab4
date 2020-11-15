4816006eccb23fc152afd12c364b2afa1de7e05f
Robbir99 <42833476+Robbir99@users.noreply.github.com>
2020-11-15 17:17:52 +0100

    Update Graph_plotter.py

--- Graph_plotter.py
+++ Graph_plotter.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 Charpy_results = [[],[]] #[[temp], [joule]]
+Charpy_results_varmebehandlet = [[],[]] #[[temp], [joule]]
 
 # Setting the path to the xlsx file:
 xlsx_file =  'Charpytest_resultater.xlsx'
@@ -18,8 +19,17 @@ for c1, c2 in cells:
     Charpy_results[0].append(c1.value)
     Charpy_results[1].append(c2.value)
 
+cells_varmebehandlet = sheet['D2': "E12"]
+for c1, c2 in cells_varmebehandlet:
+    print("{0:8} {1:8}".format(c1.value, c2.value))
+    Charpy_results_varmebehandlet[0].append(c1.value)
+    Charpy_results_varmebehandlet[1].append(c2.value)
 
-plt.plot(Charpy_results[0], Charpy_results[1])
+
+plt.plot(Charpy_results[0], Charpy_results[1],"bo", label = 'Ikke varmebehandlet')
+plt.plot(Charpy_results_varmebehandlet[0], Charpy_results_varmebehandlet[1], 'ro', label = 'varmebehandlet')
 plt.xlabel("Temperatur")
 plt.ylabel("Joule")
+plt.grid()
+plt.legend()
 plt.show()
